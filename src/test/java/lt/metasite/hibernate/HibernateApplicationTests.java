6cfa95b422236c783c7ff28281cc01f4eab143c8
juozasa <juozas.adamonis@metasite.lt>
2019-10-27 14:14:23 +0200

    Initializer
    Initializer

---
+++ src/test/java/lt/metasite/hibernate/HibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.metasite.hibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
